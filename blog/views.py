51b59d4a9f8eee57c4f309ae54ccfbfbb36b5de6
Ibnu Shidq <hia.develop@gmail.com>
2017-09-25 14:00:03 +0700

    Add style css

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
+# Create your views here.
+
+def post_list(request):
+    posts = Post.objects.filter().order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
